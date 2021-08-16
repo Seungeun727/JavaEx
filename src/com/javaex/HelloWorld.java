3a64e314be05023f3f31615380be9c8027f99b3a
노승은 <seun2230@gmail.com>
2021-08-16 10:52:42 +0900

    Initial Commit

---
+++ src/com/javaex/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.javaex; // 패키지 선언
+
+public class HelloWorld { // 공개클래스 선언부 + 클래스 이름: 클래스 선언
+	// 클래스 이름은 소스파일 이름과 동일할 것
+	public static void main(String[] args) {
+		// 메세지 출력
+		System.out.println("Hello Java!");
+	}
+}
