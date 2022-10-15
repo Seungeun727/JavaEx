a32702869410d51bf87c75381b808d0a9ee78edf
노승은 <seun2230@gmail.com>
2022-10-15 23:52:25 +0900

    feat: BOJ/일차원 배열로 소수점 3자리까지 평균 계산

---
+++ src/com/baekjoon/array/Exam06.java
@@ -0,0 +1,41 @@
+package com.baekjoon.array;
+
+import java.util.Scanner;
+public class Exam06 {
+	// 백준 문제 4534
+	// 문제 본문 링크: https://www.acmicpc.net/problem/4344
+	// 인덱스를 확인하지 않아서 (ArrayIndexOutOfBounds) 발생했음.
+	public static void main(String[] args) {
+		avgEx01();
+	}
+	// avgEx02()은 연속된 결과로 성공함
+	private static void avgEx01() {
+		Scanner scan = new Scanner(System.in);
+        int c = scan.nextInt();
+
+		for(int i = 0; i < c; i++) {
+			int n = scan.nextInt();
+			int[] scores = new int[n];
+			int cnt = 0; 
+			double sum = 0;
+			double avg = 0;
+			double percent = 0;
+			
+			for(int j = 0; j < n; j++) {
+				int grade = scan.nextInt();
+				scores[j] = grade;
+				sum += scores[j];
+			}
+			
+			avg = (double)(sum / scores.length);
+			
+			for(int k = 0; k < n; k++) {
+				if(avg < scores[k]) {
+					cnt++;
+                } 
+			 }
+			 percent = (double)(cnt * 100) / scores.length;
+			 System.out.println(String.format("%.3f",  percent) + "%");
+	    }
+	}
+}
